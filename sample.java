ed5173c3cf1e503e86cd24282ed7f35da0e54355
vijaydattu <vijaydattu.b@gmail.com>
2018-04-25 15:54:19 +0000

    tilak changesss

--- sample.java
+++ sample.java
@@ -1,7 +1,7 @@
 public  class sample {
    public static void main(String[] args) {
       // Prints "Hello, World" in the terminal window.
-      System.out.println("Hello, World");
+      System.out.println("This is Tilak");
 	System.out.println("This is seond line third line");
    }
 }
