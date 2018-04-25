8ee65a986cf605127f182819a3e8abbbe0b8e24c
vijaydattu <vijaydattu.b@gmail.com>
2018-04-25 15:17:58 +0000

    Solved

--- sample.java
+++ sample.java
@@ -2,6 +2,6 @@ public  class sample {
    public static void main(String[] args) {
       // Prints "Hello, World" in the terminal window.
       System.out.println("Hello, World");
-	System.out.println("This is seond line!!!!");
+	System.out.println("This is seond line third line");
    }
 }
