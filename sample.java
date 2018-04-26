7210c31ca96c097a97ae1074ff535785eb59cba6
vijaydattu <32859429+vijaydattu@users.noreply.github.com>
2018-04-26 10:14:18 -0500

    Delete sample.java

--- sample.java
+++
@@ -1,7 +0,0 @@
-public  class sample {
-   public static void main(String[] args) {
-      // Prints "Hello, World" in the terminal window.
-      System.out.println("This is Tilak");
-	System.out.println("This is seond line third line");
-   }
-}
